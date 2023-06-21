3a60fb6ffb7bc7b8c5a2df9db61fd60b754ba8f5
Yoel RIDGWAY-LOPEZ <yoelridgway@gmail.com>
2023-06-21 21:27:11 +0200

    created my own line by line input system

--- extractlinks.cpp
+++ extractlinks.cpp
@@ -71,58 +71,67 @@ void	scrape_url(std::string exec_command, std::list<std::string>& url_list)
 int	main(int ac, char **av)
 {
 	(void)av;
-	if (ac == 2 || ac == 3)
+	(void)ac;
+	// int			n = 0;
+	std::string	input;
+	std::string	input_msg[] = {"enter starting url:", "enter curl from the network monitor on your browser (leave blank if you don\'t know what this is):", "enter the number of pages you want to crawl:"};
+	std::list<std::string>		url_list;
+	std::vector<std::string>	visited;
+	std::list<std::string>		unique_subdomains;
+	std::string	curl_command("curl --compressed ");
+	std::string	url;
+	size_t		pages;
+	std::string	cookies;
+	// cookies.substr(cookies.find("curl"));
+	// std::stringstream	ss;
+	// std::cout << "ac: " << ac << std::endl;
+	// while (std::cin >> ss,std::getline(std::cin))
+	std::cout << input_msg[0] << std::endl;
+	std::getline(std::cin, input);
+	url = input;
+	std::cout << input_msg[1] << std::endl;
+	std::getline(std::cin, input);
+	cookies = input;
+	std::cout << input_msg[2] << std::endl;
+	std::getline(std::cin, input);
+	pages = atoi(input.c_str());
+	for (size_t i = 0; i < pages; i++)
 	{
-		std::list<std::string>		url_list;
-		std::vector<std::string>	visited;
-		std::list<std::string>		unique_subdomains;
-
-		std::string	curl_command("curl --compressed ");
-		std::string	url(av[1]);
-		std::string	cookies(" ");
-		if (ac == 3)
-			cookies.append(av[2]);
-		
-		for (size_t i = 0; i < 100; i++)
-		{
-			if (url.empty() || url_list.empty())
-				std::cout << "\n----------------------------\n"
-						  << "ALL PAGES HAVE BEEN SCRAPED!"
-						  << "\n----------------------------\n";
-			scrape_url(curl_command + url + cookies, url_list);
-			url_list.remove_if(isnt_domain);
-			url_list.sort();
-			url_list.unique();
-			url_list.remove(url);
-			visited.push_back(url);
-			while (std::find(visited.begin(), visited.end(), url_list.front()) != visited.end())
-				url_list.pop_front();
-			url = url_list.front();
-			srand(time(NULL));
-			size_t j = 100 + (rand() % 1000000);
-			std::cout << "j: " << j << std::endl;
-			usleep(j);
-		}
-
-		std::cout << "\n------------------SCRAPED URLS----------------\n";
-		for (size_t i = 0; i < visited.size(); i++)
-			std::cout << visited[i] << std::endl;
-		std::cout << "----------------------------------------------\n";
-
-		for (size_t i = 0; i < visited.size(); i++)
-		{
-			std::string	subdomain;
-			size_t n = visited[i].find("42.fr") + 5;
-			unique_subdomains.push_back(visited[i].substr(0, n));
-		}
-		unique_subdomains.sort();
-		unique_subdomains.unique();
-		std::cout << "\n---------------UNIQUE SUBDOMAINS--------------\n";
-		for (std::list<std::string>::iterator it = unique_subdomains.begin(); it != unique_subdomains.end(); it++)
-			std::cout << *it << std::endl;
-		std::cout << "----------------------------------------------\n";
+		if (i > 0 && (url.empty() || url_list.empty()))
+			std::cout << "\n----------------------------\n"
+						<< "ALL PAGES HAVE BEEN SCRAPED!"
+						<< "\n----------------------------\n";
+		scrape_url(curl_command + url + cookies, url_list);
+		url_list.remove_if(isnt_domain);
+		url_list.sort();
+		url_list.unique();
+		url_list.remove(url);
+		visited.push_back(url);
+		while (std::find(visited.begin(), visited.end(), url_list.front()) != visited.end())
+			url_list.pop_front();
+		url = url_list.front();
+		srand(time(NULL));
+		// size_t j = 100 + (rand() % 1000000);
+		// std::cout << "j: " << j << std::endl;
+		// usleep(j);
 	}
-	else
-		std::cerr << "please add input url to scrape as first argument\n";
+
+	std::cout << "\n------------------SCRAPED URLS----------------\n";
+	for (size_t i = 0; i < visited.size(); i++)
+		std::cout << visited[i] << std::endl;
+	std::cout << "----------------------------------------------\n";
+
+	for (size_t i = 0; i < visited.size(); i++)
+	{
+		std::string	subdomain;
+		size_t n = visited[i].find("42.fr") + 5;
+		unique_subdomains.push_back(visited[i].substr(0, n));
+	}
+	unique_subdomains.sort();
+	unique_subdomains.unique();
+	std::cout << "\n---------------UNIQUE SUBDOMAINS--------------\n";
+	for (std::list<std::string>::iterator it = unique_subdomains.begin(); it != unique_subdomains.end(); it++)
+		std::cout << *it << std::endl;
+	std::cout << "----------------------------------------------\n";
 	return (0);
 }
--- run.sh
+++ run.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-c++ -Wall -Werror -Wextra -g3 extractlinks.cpp -o spider&& ./spider $1 $2
+echo enter starting url:
+read url
+echo enter curl from the network monitor on your browser \(leave blank if you don\'t know what this is\):
+read curl
+echo enter the number of pages you want to crawl:
+read pages
+
+c++ -Wall -Werror -Wextra -g3 extractlinks.cpp -o spider&& ./spider $url $pages $curl
+
+#I'm not sure what I'm doing wrong. I've tried using the -std=c++11 flag, but that doesn't seem to work either. I'm using g++ 4.8.2 on Ubuntu 14.04. Any help would be appreciated. Thanks!
+
+
